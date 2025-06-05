67c78f71c95979b27a47cee0cda4124e9eae0212
Caua <cauamasseu@gmail.com>
2025-06-04 21:37:50 -0300

    att2

--- code.R
+++ code.R
@@ -1,19 +1,28 @@
+library(robustbase)
+
 set.seed(1)
+n <- 100
 
-n <- 300
+beta <- c(0.6, 2, 3)
+x1 <- rnorm(n = n, sd = 1)
+x2 <- rnorm(n = n, sd = 1)
 
-x1 <- rnorm(n)
-x2 <- rnorm(n)
-beta <- c(0.5, 1, 2)
+#Put a outlier in x_1 and x_2 in different times
+size = 10
+#x1[n/2] <- x1[n/2] + size
+#x2[3*n/4] <- x2[3*n/4] + size
 
-y <- beta[1] + beta[2]*x1 + beta[3]*x2 + rnorm(n)
+X <- matrix(c(rep(1, n), x1, x2), nrow = n)
+y <- X %*% beta + rnorm(n)
 
-#Put outlier in the model
-size <- 7.5
+y[n/5] <- 20*y[n/5]
+y[n/4] <- 20*y[n/4]
 
-x1[n/2] <- x1[n/2] + size
-x2[2*n/3] <- x2[2*n/3] + size
 
-y <- beta[1] + beta[2]*x1 + beta[3]*x2 + rnorm(n)
+plot(y ~x1)
+hist(y)
 
 lm(y ~ 1 + x1 + x2)
+coef(lmrob.lar(y = y, x = X))
+MASS::rlm(y = y, x = X)
+lmrob.S(x = X, y = y, control = lmrob.control("KS2011", max.it = 1000))[1]
